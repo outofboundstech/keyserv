e2d445ce95bc1938d6cb97b6562d14f5eaf4c48c
Marcel Oomens <mail@marceloomens.com>
2017-06-10 14:39:34 +0200

    Minor edits
    Email formatting (headers; body)
    
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- web/mailman.ex
+++ web/mailman.ex
@@ -5,11 +5,10 @@ defmodule Keyserv.Mailman do
 
   alias ExPosta.Message
 
-  def deliver(conn, _params=%{"fingerprint" => fingerprint, "from" => from, "subject" => subject, "text" => text}) do
+  def deliver(conn, _params=%{"fingerprint" => fingerprint, "content" => body}) do
     # Lookup email address by fingerprint
     key = Repo.get_by!(Key, fingerprint: fingerprint)
-    # Send my email (to, subject, text)
-    msg = Message.new(text: text, reply_to: from, to: [key.email], subject: subject)
+    msg = Message.new(text: body, to: [key.email], subject: "CryptoForm: new message notification")
     pid=ExPosta.send msg
     IO.inspect Task.await(pid)
 
