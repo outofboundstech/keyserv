1e0591536571032b0cd180303c06d8fffbcd5306
Marcel Oomens <mail@marceloomens.com>
2017-08-20 09:15:22 +0200

    Update ependencies
    Update to Elixir v1.5 and Phoenix 1.3. I still need to update my project structure according to http://phoenixframework.org/blog/upgrading-from-120-to-130
    
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- mix.exs
+++ mix.exs
@@ -30,19 +30,20 @@ defmodule Keyserv.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_html, "~> 2.10"},
+     {:phoenix_live_reload, "~> 1.1", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 3.0"},
+     {:comeonin, "~> 4.0"},
+     {:bcrypt_elixir, "~> 0.12.0"},
      {:poison, "~> 3.0", override: true},
-     {:cors_plug, "~> 1.3"},
+     {:cors_plug, "~> 1.4"},
      {:gen_smtp, "~> 0.12.0"},
-     {:distillery, "~> 1.4", runtime: false}]
+     {:distillery, "~> 1.5", runtime: false}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
